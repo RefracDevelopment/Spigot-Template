2bd0c96cbd6010d93975b3280191ffe114f71536
Zachary Baldwin <refracplaysmc@gmail.com>
2023-08-14 03:53:05 -0500

    Fix uuid

--- src/main/java/me/refracdevelopment/example/utilities/Utilities.java
+++ src/main/java/me/refracdevelopment/example/utilities/Utilities.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class Utilities {
 
     public static final UUID getDevUUID = UUID.fromString("d9c670ed-d7d5-45fb-a144-8b8be86c4a2d");
-    public static UUID getDevUUID2 = UUID.fromString("ab898e40-9088-45eb-9d69-e0b78e872627");
+    public static final UUID getDevUUID2 = UUID.fromString("ab898e40-9088-45eb-9d69-e0b78e872627");
 
     public static void sendDevMessage(Player player) {
         final LocaleManager locale = ExamplePlugin.getInstance().getManager(LocaleManager.class);
