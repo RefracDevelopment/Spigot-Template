7bfbbed6bd498963ed75fdbc250212554092f9ef
Zachary Baldwin <zacharybaldwin904@gmail.com>
2023-10-17 18:34:03 -0500

    Update Color

--- src/main/java/me/refracdevelopment/example/utilities/chat/Color.java
+++ src/main/java/me/refracdevelopment/example/utilities/chat/Color.java
@@ -21,7 +21,7 @@ public class Color extends Manager {
     public String translate(CommandSender sender, String source) {
         source = placeholders.setPlaceholders(sender, source);
 
-        if (sender instanceof Player) {
+        if (sender instanceof Player && plugin.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             return PlaceholderAPI.setPlaceholders((Player) sender, translate(source));
         } else return translate(source);
     }
@@ -30,16 +30,18 @@ public class Color extends Manager {
         return hexUtils.colorify(source);
     }
 
-    public void sendMessage(CommandSender sender, String message) {
-        if (message.contains("%empty%") || message.equalsIgnoreCase("%empty%") || message.isEmpty()) return;
+    public void sendMessage(CommandSender sender, String source) {
+        if (plugin.getConfigFile().getString(source).contains("%empty%") ||
+                plugin.getConfigFile().getString(source).equalsIgnoreCase("%empty%") ||
+                plugin.getConfigFile().getString(source).isEmpty()) return;
 
-        sender.sendMessage(translate(sender, plugin.getConfigFile().getString("messages." + message).replace("%cmd%", plugin.getCommands().EXAMPLE_COMMAND_ALIASES.get(0))));
+        sender.sendMessage(translate(sender, plugin.getConfigFile().getString(source)));
     }
 
-    public void sendCustomMessage(CommandSender sender, String message) {
-        if (message.contains("%empty%") || message.equalsIgnoreCase("%empty%") || message.isEmpty()) return;
+    public void sendCustomMessage(CommandSender sender, String source) {
+        if (source.contains("%empty%") || source.equalsIgnoreCase("%empty%") || source.isEmpty()) return;
 
-        sender.sendMessage(translate(sender, message));
+        sender.sendMessage(translate(sender, source));
     }
 
     public void log(String message) {
