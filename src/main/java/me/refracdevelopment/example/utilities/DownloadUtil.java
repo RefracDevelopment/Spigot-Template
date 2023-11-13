22d4947a47c20f7a4ffa67a608b94139fb9dad6b
Zachary Baldwin <zacharybaldwin904@gmail.com>
2023-11-13 05:08:58 -0600

    Added MariaDB to DownloadUtil

--- src/main/java/me/refracdevelopment/example/utilities/DownloadUtil.java
+++ src/main/java/me/refracdevelopment/example/utilities/DownloadUtil.java
@@ -16,11 +16,16 @@ public class DownloadUtil {
                 .version("2.0.9")
                 .build();
         Library lib2 = Library.builder()
+                .groupId("org{}mariadb{}jdbc") // "{}" is replaced with ".", useful to avoid unwanted changes made by maven-shade-plugin
+                .artifactId("mariadb-java-client")
+                .version("3.2.0")
+                .build();
+        Library lib3 = Library.builder()
                 .groupId("org{}xerial") // "{}" is replaced with ".", useful to avoid unwanted changes made by maven-shade-plugin
                 .artifactId("sqlite-jdbc")
                 .version("3.43.0.0")
                 .build();
-        Library lib3 = Library.builder()
+        Library lib4 = Library.builder()
                 .groupId("com{}j256{}ormlite") // "{}" is replaced with ".", useful to avoid unwanted changes made by maven-shade-plugin
                 .artifactId("ormlite-jdbc")
                 .version("6.1")
@@ -30,5 +35,6 @@ public class DownloadUtil {
         libraryManager.loadLibrary(lib);
         libraryManager.loadLibrary(lib2);
         libraryManager.loadLibrary(lib3);
+        libraryManager.loadLibrary(lib4);
     }
 }
